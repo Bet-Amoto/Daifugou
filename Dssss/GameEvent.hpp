d29f40225bb7790a3d050c5a338a5a686dc2f4ff
tabei <74161279+Bet-Amoto@users.noreply.github.com>
2022-09-15 14:13:56 +0900

    create

---
+++ Dssss/GameEvent.hpp
@@ -0,0 +1,221 @@
+﻿#pragma once
+# include <Siv3D.hpp>
+using namespace PlayingCard;
+
+class GameEvent {
+public:
+
+	GameEvent() {
+		reset();
+	}
+
+	void reset() {
+		opencards.clear();
+		topcards.clear();
+		suittight = false;
+		kaidantight = false;
+		multitight = false;
+		kakumei = false;
+		nocard = true;
+		supesan = false;
+		yagiri = false;
+	}
+	void restart() {
+		topcards.clear();
+		suittight = false;
+		kaidantight = false;
+		multitight = false;
+		nocard = true;
+		supesan = false;
+		yagiri = false;
+	}
+	//場にカードがなければtrue
+	bool isrestart() const { return nocard; }
+	//スート縛りがあるか
+	bool issuittight() const { return suittight; }
+	//階段縛りがあるか
+	bool iskaidantight() const { return kaidantight; }
+	//複数枚縛りがあるか
+	bool ismultitight() const { return multitight; }
+	//革命起きてるか
+	bool iskakumei() const { return kakumei; }
+
+	bool isyagiri() { return yagiri; }
+	bool issupesan() { return supesan; }
+	//今の一番上にアルカード
+	Array<Card> topcard() const { return topcards; }
+	//これまでに場に出されたカード
+	Array<Card> openedcards() const { return opencards; }
+
+	bool checkcard(Array<Card> setcard) const {
+		sortcard(setcard);
+		//場にカードがなければtrue
+		if (isrestart())return true;
+		//カードの枚数が違ったらfalse
+		if (topcard().size() != setcard.size())return false;
+		//スぺ3返しでtrue
+		if (setcard.size() == 1 and ((setcard[0].isSpade() and setcard[0].rank == 3) and topcard()[0].isJoker()))return true;
+		//一番弱いカード同士を比較してsetcardが弱かったらfalse
+		if (not compareStrange(setcard[0], topcard()[0]))return false;
+		//階段縛りで階段じゃなかったらfalse
+		if (iskaidantight() and not iskaidan(setcard))return false;
+		//複数枚縛り守らなかったらfalse
+		if (ismultitight() and (not ismulti(setcard) or setcard[0].rank <= topcard()[0].rank))return false;
+		//スート縛り守らなかったらfalse
+		if (issuittight() and not comparesuit(setcard, topcard())) return false;
+
+		return true;
+	}
+	bool checkcard(Card setcard) const { Array<Card> tmp{ setcard }; return checkcard(tmp); }
+
+
+	//@return -1...エラー 0...通常 1...スート縛り発生 2...複数枚縛り発生 3...階段縛り発生 4...革命発生 100...パス
+	int setcard(Array<Card> setcard) {
+		if (setcard.size() == 0)return 100;
+		sortcard(setcard);
+		if (not checkcard(setcard))return -1;
+		int ev = 0;
+		if (isrestart()) {
+			if (setcard.size() >= 2) {
+				//階段なら縛り発動
+				if (iskaidan(setcard)) {
+					kaidantight = true;
+					ev = 3;
+				}
+				//複数枚出すと縛り発動
+				if (ismulti(setcard)) {
+					multitight = true;
+					ev = 2;
+					//4枚以上同じで革命
+					if (setcard.size() >= 4) {
+						kakumei = true;
+						sortcard(setcard);
+						ev = 4;
+					}
+				}
+			}
+			nocard = false;
+		}
+		//前のカードとスートが同じなら縛り発動
+		else {
+			if (comparesuit(topcards, setcard, true) and not issuittight()) {
+				suittight = true;
+				ev = 1;
+			}
+			if (topcard().size() == 1 && topcard()[0].suit == Suit::Joker && setcard[0].rank == 3 && setcard[0].suit == Suit::Spade)supesan = true;
+		}
+
+		if (not iskaidantight() and setcard[0].rank==8 ) yagiri = true;
+		topcards = setcard;
+		opencards.insert(opencards.end(), setcard.begin(), setcard.end());
+		return ev;
+	}
+	//左が弱ければtrue
+	bool compareStrange(Card a, Card b) const {
+		if (a.isJoker())return true;
+		if (b.isJoker())return false;
+		if (a.rank == 1 || a.rank == 2)a.rank += 13;
+		if (b.rank == 1 || b.rank == 2)b.rank += 13;
+		return iskakumei() ? a.rank < b.rank : a.rank>b.rank;
+	}
+
+	//カードを弱い順にソート
+	void sortcard(Array<Card>& cards) const {
+		if (not iskakumei()) {
+			cards.sort_by([](Card a, Card b) {
+				if (a.isJoker())return false;
+				if (b.isJoker())return true;
+				if (a.rank == 1 || a.rank == 2)a.rank += 13;
+				if (b.rank == 1 || b.rank == 2)b.rank += 13;
+				return a.rank < b.rank; });
+		}
+		else {
+			cards.sort_by([](Card a, Card b) {
+				if (a.isJoker())return false;
+				if (b.isJoker())return true;
+				if (a.rank == 1 || a.rank == 2)a.rank += 13;
+				if (b.rank == 1 || b.rank == 2)b.rank += 13;
+				return a.rank > b.rank; });
+		}
+	}
+
+	//カードのスートの比較
+	bool comparesuit(Array<Card> a, Array<Card> b, bool nojoker = false) const {
+		sortcard(a);
+		sortcard(b);
+		size_t jokersA = a.size();
+		a.remove_if([](Card x) {return x.isJoker(); });
+		jokersA -= a.size();
+		if (nojoker and jokersA > 0)return false;
+		size_t jokersB = b.size();
+		b.remove_if([](Card x) {return x.isJoker(); });
+		jokersB -= b.size();
+		if (nojoker and jokersB > 0)return false;
+		a.sort_by([](Card x, Card y) {return x.suit < y.suit; });
+		b.sort_by([](Card x, Card y) {return x.suit < y.suit; });
+		int32 i = 0, j = 0;
+		while (i < a.size() && j < b.size()) {
+			if (a[i].suit < b[j].suit) {
+				if (jokersB > 0) {
+					jokersB--;
+					i++;
+				}
+				else return false;
+			}
+			else if (a[i].suit > b[j].suit) {
+				if (jokersA > 0) {
+					jokersA--;
+					j++;
+				}
+				else return false;
+			}
+			else {
+				j++;
+				i++;
+			}
+		}
+		return true;
+	}
+
+	//全部同じ数字か
+	bool ismulti(Array<Card> cards) const {
+		if (cards.size() < 2)return false;
+		sortcard(cards);
+		int num = cards[0].rank;
+		for (auto card : cards) {
+			if (not num == card.rank and not card.isJoker())return false;
+		}
+		return true;
+	}
+
+	//階段になってるか
+	bool iskaidan(Array<Card> cards) const {
+		if (cards.size() < 3)return false;
+		sortcard(cards);
+		int32 jokers = (int32)cards.size();
+		cards.remove_if([](Card a) {return a.isJoker(); });
+		jokers -= (int32)cards.size();
+		int32 suits = cards[0].suit;
+		for (auto i : step(cards.size() - 1)) {
+			if (cards[i + 1].suit != suits)return false;
+			if (cards[i + 1].rank - cards[i].rank != 1) {
+				if (jokers <= 0)return false;
+				jokers--;
+				cards[i].rank++;
+				i--;
+			}
+		}
+		return true;
+	}
+private:
+	bool nocard;
+	bool suittight;
+	bool kaidantight;
+	bool multitight;
+	bool kakumei;
+	bool yagiri;
+	bool supesan;
+	Array<Card> topcards;
+	Array<Card> opencards;
+
+};
---
+++ Dssss/Main.cpp
@@ -0,0 +1,277 @@
+﻿# include <Siv3D.hpp>
+#include "GameEvent.hpp"
+using namespace PlayingCard;
+
+class Player {
+public:
+	Player(String name) { playername = name; };
+	virtual Array<Card> initplayer(const Array<Card>& mycard,int givecards) = 0;
+	virtual Array<Card> action(const GameEvent& GameEvent,const Array<Card>& mycard) = 0;
+	String getname() const { return playername; }
+
+private:
+	String playername;
+};
+
+class  Gamemastar {
+public:
+	Gamemastar(Array<Player*> ps) {
+		pack = Pack{ 75, Palette::Red };
+		players = ps;
+		playercard = Array<Array<Card>>(4, Array<Card>());
+		makeyamafuda(true);
+		ranking = Array<int32>{ 0,1,2,3 };
+		font = Font(20);
+	}
+	//山札を作る
+	void makeyamafuda(bool setblind = false) {
+		if (not setblind) {
+			yamafuda.shuffle();
+			events = GameEvent();
+			return;
+		}
+		//公式ルールに基づいてシャッフル
+		Array<Card> cards = CreateDeck(0);
+		blindcard.clear();
+		cards.shuffle();
+		blindcard << cards.back();
+		cards.pop_back();
+		blindcard << cards.back();
+		cards.pop_back();
+		cards << Card(Suit::Joker, 50) << Card(Suit::Joker, 50);
+		cards.shuffle();
+		yamafuda = cards;
+	}
+	void initgame() {
+		gamecount++;
+		countturn = 0;
+		if (gamecount > 4) {
+			setcount++;
+			gamecount = 1;
+			makeyamafuda(true);
+			players.shuffle();
+			ranking = Array<int32>{ 0,1,2,3 };
+			nowturn = 0;
+		}
+		else {
+			makeyamafuda(false);
+			nowturn = ranking[3];
+		}
+		finish = Array<int32>(4, 0);
+		events.reset();
+		for (auto& pc : playercard)pc.clear();
+		//手札配給
+		for (auto i : step(13)) {
+			playercard[0] << yamafuda[4 * i];
+			playercard[1] << yamafuda[4 * i + 1];
+			playercard[2] << yamafuda[4 * i + 2];
+			playercard[3] << yamafuda[4 * i + 3];
+		}
+
+		//全員の手札を弱い順にソート
+		for (auto& pc : playercard)events.sortcard(pc);
+
+		//順位でカード交換
+		Array<Array<Card>> givecards(4, Array<Card>());
+		givecards[0] = players[ranking[0]]->initplayer(playercard[ranking[0]], gamecount == 1 ? 0 : 2);
+		if (gamecount != 1 and (givecards[0].size()!=2 or not usecard(ranking[0], givecards[0])))throw Error(U"Player0 = init Error");
+		givecards[1] = players[ranking[1]]->initplayer(playercard[ranking[1]], gamecount == 1 ? 0 : 1);
+		if (gamecount != 1 and (givecards[1].size() != 1 or not usecard(ranking[1], givecards[1])))throw Error(U"Player1 = init Error");
+		players[ranking[2]]->initplayer(playercard[ranking[2]], 0);
+		players[ranking[3]]->initplayer(playercard[ranking[3]], 0);
+		if (gamecount != 1) {
+			givecards[2] << playercard[ranking[2]].back();
+			usecard(ranking[2], playercard[ranking[2]].back());
+			givecards[3] << playercard[ranking[3]].back();
+			usecard(ranking[3], playercard[ranking[3]].back());
+			givecards[3] << playercard[ranking[3]].back();
+			usecard(ranking[3], playercard[ranking[3]].back());
+			for (auto i : step(4)) playercard[ranking[i]].append(givecards[3 - i]);
+		}
+	}
+	void progress(){
+		countturn++;
+		if (nowturn == -1)return;
+		if (finish[nowturn] != 0)return;
+		Array<Card> action = players[nowturn]->action(events, playercard[nowturn]);
+		int e = events.setcard(action);
+		if (e == -1 or not usecard(nowturn,action)) {
+			finish[nowturn] = -1;
+			ranking[4 - countplayer(-1)] = nowturn;
+			Print << U"Player{} 反則負け(ランク:{} ターン{})"_fmt(players[nowturn]->getname(),rankname[4 - countplayer(-1)], countturn);
+			return;
+		}
+		if (e == 1)Print << U"スート縛り発生(ターン{})"_fmt(countturn);
+		if (e == 2)Print << U"複数枚出し縛り発生(ターン{})"_fmt(countturn);
+		if (e == 3)Print << U"階段縛り発生(ターン{})"_fmt(countturn);
+		if (e == 4)Print << U"革命発生(ターン{})"_fmt(countturn);
+		if (e == 100) {
+			Print<< U"Player{} パス(ターン{})"_fmt(players[nowturn]->getname(),countturn);
+			passcount++;
+		}
+		else passcount = 0;
+		if (passcount >= countplayer(0)-1) {
+			events.restart();
+			Print<< U"パスが続いたため場が流れます(ターン{})"_fmt(countturn);
+			passcount = 0;
+		}
+		if (playercard[nowturn].size() <= 0) {
+			finish[nowturn] = 1;
+			ranking[countplayer(1)-1] = nowturn;
+			Print << U"Player{} 勝ち抜け(ランク:{} ターン{})"_fmt(players[nowturn]->getname(),rankname[countplayer(1) - 1], countturn);
+		}
+		if (events.isyagiri()) {
+			Print << U"Player{} 8切り発動(ターン{})"_fmt(players[nowturn]->getname(),countturn);
+			nowturn--;
+			events.restart();
+		}
+		if (events.issupesan()) {
+			Print << U"Player{} スぺ3返し!(ターン{})"_fmt(players[nowturn]->getname(), countturn);
+			nowturn--;
+			events.restart();
+		}
+		nowturn = nextturn(nowturn);
+	}
+	bool isfinish() {
+		return countplayer(0) == 0;
+	}
+	void draw() const{
+		Line(900, 0, 900, 720).draw(Palette::Yellowgreen);
+		for (auto p : step(players.size())) {
+			font(players[p]->getname()).drawAt(Vec2(100, 150 * p + 150),p==nowturn ? Palette::Lightpink : Palette::Black);
+		}
+		for (auto p : step(playercard.size())) {
+			for (auto c : step(playercard[p].size())) {
+				pack(playercard[p][c]).drawAt(Vec2(50*c + 200, 150 * p + 150));
+			}
+		}
+		for (auto p : step(events.topcard().size())) {
+			pack(events.topcard()[p]).drawAt(Vec2(1100+30*p,350));
+		}
+	}
+	Array<Player*> getRanking() {
+		Array<Player*> p;
+		for (auto r : ranking) {
+			p << players[r];
+		}
+		return p;
+	}
+private:
+	int32 nextturn(int32 n) {
+		if (countplayer(0) == 0)return -1;
+		n = (n + 1) % 4;
+		while (finish[n] != 0)n = (n + 1) % 4;
+		return n;
+	}
+	bool ishave(int who, Array<Card> cards) {
+		for (auto card : cards) {
+			if (not ishave(who, card))return false;
+		}
+		return true;
+	}
+	bool ishave(int who, Card card) {
+		auto itr = find(playercard[who].begin(), playercard[who].end(), card);
+		return itr != playercard[who].end();
+	}
+	bool usecard(int who,Array<Card> cards) {
+		if (not ishave(who, cards))return false;
+		for (auto card : cards)usecard(who, card);
+		return true;
+	}
+	bool usecard(int who, Card card) {
+		auto itr = find(playercard[who].begin(), playercard[who].end(), card);
+		if (itr == playercard[who].end())return false;
+		playercard[who].erase(itr);
+		return true;
+	}
+	int32 countplayer(int32 c) {
+		int co = 0;
+		for (auto f : finish)if (f == c)co++;
+		return co;
+	}
+
+	Pack pack;
+	GameEvent events;
+	int32 gamecount = 4;
+	int32 setcount = 0;
+	int32 nowturn = 0;
+	int32 countturn = 0;
+	int32 passcount = 0;
+	Font font;
+	Array<Card> yamafuda;
+	Array<Card> blindcard;
+	Array<Array<Card>> playercard;
+	Array<Player*> players;
+	Array<int32> ranking;
+	Array<int32> finish;
+	const Array<String> rankname{ U"大富豪",U"富豪", U"貧民", U"大貧民" };
+};
+
+class randombot :public Player {
+public:
+	randombot() :Player(U"RandomBot") {};
+	Array<Card> initplayer(const Array<Card>& mycard, int givecards) override{
+		if (givecards == 2)return Array<Card>{mycard[0], mycard[1]};
+		if (givecards == 1)return Array<Card>{mycard[0]};
+		return Array<Card>();
+	}
+	Array<Card> action(const GameEvent& gameevent, const Array<Card>& mycard) override{
+		Array<Card> tmp = Array<Card>();
+		for (auto p : mycard)if (gameevent.checkcard(p))tmp << p;
+		if(tmp.size()==0)return Array<Card>();
+		Card n = tmp.choice();
+		return Array<Card>{n};
+	}
+private:
+};
+
+class mybot :public Player {
+public:
+	mybot() :Player(U"MyBot") {};
+	Array<Card> initplayer(const Array<Card>& mycard, int givecards) override {
+		if (givecards == 2)return Array<Card>{mycard[0], mycard[1]};
+		if (givecards == 1)return Array<Card>{mycard[0]};
+		return Array<Card>();
+	}
+	Array<Card> action(const GameEvent& gameevent, const Array<Card>& mycard) override {
+		Array<Card> tmp = Array<Card>();
+		for (auto p : mycard)if (gameevent.checkcard(p))tmp << p;
+		if (tmp.size() == 0)return Array<Card>();
+		gameevent.sortcard(tmp);
+		Card n = tmp.front();
+		return Array<Card>{n};
+	}
+private:
+};
+
+void Main()
+{
+	Window::Resize(1280, 720);
+	Scene::SetBackground(Palette::Darkgreen);
+	const Array<String> rankname{ U"大富豪",U"富豪", U"貧民", U"大貧民" };
+	Array<Player*> players{ (new mybot()),(new randombot()) ,(new randombot()) ,(new randombot()) };
+	Gamemastar mastar(players);
+	mastar.initgame();
+	const double cooltime = 1;
+	double t = 0;
+	while (System::Update())
+	{
+		t += Scene::DeltaTime();
+		if (t > cooltime) {
+			if (mastar.isfinish()) {
+				ClearPrint();
+				Print << U"ゲームが終了しました";
+				Array<Player*> ranking = mastar.getRanking();
+				for (auto p : step(ranking.size())) {
+					Print << U"{}:{}"_fmt(rankname[p], ranking[p]->getname());
+				}
+				mastar.initgame();
+				t = 0; continue;
+
+			}
+			mastar.progress();
+			t = 0;
+		}
+		mastar.draw();
+	}
+}
